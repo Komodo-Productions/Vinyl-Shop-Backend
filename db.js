3f46e3f1c836d8dba43b9616a41b176c63435b4f
JuanRuano1247723 <123260280+JuanRuano1247723@users.noreply.github.com>
2025-11-12 12:13:16 -0600

    Refactor database connection to use environment variables

--- db.js
+++ db.js
@@ -1,6 +1,16 @@
-const knex = require('knex');
-const config = require('./knexfile');
+// db.js
+const knex = require("knex");
+require("dotenv").config();
 
-const db = knex(config.development);
+const db = knex({
+    client: "mysql2",
+    connection: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    },
+});
 
 module.exports = db;
