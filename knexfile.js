61c63a9eb5794fc727b3cdcff56cbf7365a66bcd
JuanRuano1247723 <123260280+JuanRuano1247723@users.noreply.github.com>
2025-11-12 12:12:25 -0600

    Refactor knexfile to use environment variables
    Updated database connection settings to use environment variables.

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,16 @@
+require('dotenv').config();
+
 module.exports = {
-    development: {
-        client: 'mysql2',
-        connection: {
-            host: '127.0.0.1',
-            port:'3307',
-            user: 'root',
-            password: '1186023DEHG',
-            database: 'KomodoDB'
-        },
-        migrations: {
-            directory: './migrations'
-        }
+    client: 'mysql2',
+    connection: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    },
+    migrations: {
+        tableName: 'knex_migrations',
+        directory: './migrations'
     }
 };
